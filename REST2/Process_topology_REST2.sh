afff4f42e811d9b31355b7457370d4959f347427
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2021-02-06 12:08:17 +0530

    Selects HOT atoms

--- REST2/Process_topology_REST2.sh
+++ REST2/Process_topology_REST2.sh
@@ -1,16 +1,25 @@
 #-------------------------------------------------------------------------------#
 #    Bash script to Process the topology for REST2 setup
-#    Selects the hot atoms for RESt2
+#    Selects the HOT atoms for REST2
 #
 #    Author: Anji Babu Kapakayala
 #             IIT Kanpur, India.
+#
+#    USAGE : bash Process_topology_REST2.sh topol.top
 #-------------------------------------------------------------------------------#
 #!/bin/bash
-#file="unfolded.top"
-file="$1"
+infile="$1"
+#infile="trp_processed.top"
 resid=0
-while read line;do
 
+#---> Setting output filename
+fname=`echo "$infile"|cut -d "." -f1`
+outfile="${fname}_processed.top"
+
+#---> Make lines starts with * to ;*
+sed -i "s/\*/;\*/g" $infile
+
+while read line;do
 #---->
 res=`echo $line|awk '{print $2}'`
 if [ "$res" == 'residue' ] 
@@ -20,7 +29,6 @@ resid_ref=$resid
 fi
 #<---
 resid_new=`echo $line|awk '{print $3}'`
-
 if [ "$line" != "" ];then
    if [ "$resid_new" != "" ];then
       if [ "$resid_new" == "$resid_ref" ];then
@@ -36,19 +44,18 @@ if [ "$line" != "" ];then
 	 C11=`echo $line|awk '{print $11}'`
 
             if [ "$res" != 'residue' ] ;then
-#               echo "  $C1  ${C2}_ $resid_new $C4 $C5 $C6 $C7 $C8 $C9 $C10 $C11 " >> unfolded_scaled.top
-               echo $C1$'\t'${C2}_$'\t'$resid_new$'\t'$C4$'\t'$C5$'\t'$C6$'\t'$C7$'\t'$C8$'\t'$C9$'\t'$C10$'\t' $C11  >> unfolded_scaled.top
+               echo $C1$'\t'${C2}_$'\t'$resid_new$'\t'$C4$'\t'$C5$'\t'$C6$'\t'$C7$'\t'$C8$'\t'$C9$'\t'$C10$'\t' $C11  >> $outfile
             else
-               echo $line >> unfolded_scaled.top
+               echo $line >> $outfile
             fi
       else
-	 echo $line >> unfolded_scaled.top
+	 echo $line >> $outfile
       fi
    else
-      echo $line >> unfolded_scaled.top
+      echo $line >> $outfile
    fi
 else
-echo $line >> unfolded_scaled.top
+echo $line >> $outfile
 fi
 
 first=`echo $line|awk '{print $1}'`
@@ -56,16 +63,11 @@ if [ "$first" == "[" ];then
 #resid_new="X"
 resid_ref="Y"
 fi
-done<$file
+done<$infile
 
-
-#awk '{for(i=1; i<=NR; i++) 
-#{if($i=="; residue") print $0
-
-
-
-
-#}#if
-
-#}' $file
-
+#---> Make lines starts with ;* to *
+sed -i "s/;\*/\*/g" $infile
+sed -i "s/;\*/\*/g" $outfile
+#---------------------------------------------------------#
+#      <========== ANJI BABU KAPAKAYALA ==========>
+#---------------------------------------------------------#
